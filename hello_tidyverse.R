0090bda95c3a21c0a5eb755e10e8d30f3d76246c
YusukeMakino <makino-yusuke730@g.ecc.u-tokyo.ac.jp>
2020-08-19 14:59:16 +0900

    部分的変更

--- hello_tidyverse.R
+++ hello_tidyverse.R
@@ -2,5 +2,9 @@ library(tidyverse)
 library(nycflights13)
 flights %>% 
   group_by(dest) %>% 
-  summarise(mean_delay=mean(arr_delay,na.rm = TRUE)) %>% 
-  arrange(desc(mean_delay))
+  summarise(mean_arr_delay=mean(arr_delay,na.rm = TRUE)) %>% 
+  arrange(desc(mean_arr_delay))
+flights %>% 
+  group_by(origin) %>% 
+  summarise(mean_dep_delay=mean(dep_delay,na.rm = TRUE)) %>% 
+  arrange(desc(mean_dep_delay))
