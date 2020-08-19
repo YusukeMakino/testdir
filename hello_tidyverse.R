684ab740fee7e8515a39e27ec985f603db8b86ab
YusukeMakino <makino-yusuke730@g.ecc.u-tokyo.ac.jp>
2020-08-19 14:53:51 +0900

    目的地ごとの平均遅延

--- hello_tidyverse.R
+++ hello_tidyverse.R
@@ -1,1 +1,6 @@
-tidyverse::tidyverse_logo()
+library(tidyverse) 
+library(nycflights13)
+flights %>% 
+  group_by(dest) %>% 
+  summarise(mean_delay=mean(arr_delay,na.rm = TRUE)) %>% 
+  arrange(desc(mean_delay))
